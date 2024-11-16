60da27bb0fa1c2c17cb58cb65546cfcec4a7ec9d
Diya Mehra <121515496+Diya28-mehra@users.noreply.github.com>
2024-11-16 09:05:28 +0000

    More Files

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,24 @@
+// console.log("2">1); //True
+// console.log("02">1); ??True
+
+// console.log(null>0); // False
+// console.log(null==0); //False
+// console.log(null>=0); //True
+//In the last conversion null is converted to 0 
+
+console.log(undefined == 0); //False
+console.log(undefined>0); //False
+console.log(undefined<0); //False
+
+console.log("2"===2); //Checks the datatype as well as value
+console.log("1"==1); //Only checks the value
+
+console.log("23">1); //Converts the string into number and then compares
+console.log(12<"03");
+
+console.log(12<"7");
+
+console.log(12<"1");
+
+
+
---
+++ 01_Basics/operations.js
@@ -0,0 +1,27 @@
+//**********Operations ******** */
+let value = 3;
+let neg = -value;
+console.log(neg);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+
+
+let str1 = "hello";
+let str2 = "diya";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+1+"2");
+console.log("1"+1+2);
+// First If numbers are given simply add them and then if string comes take everything from that place
+
+let ans =1 ;
+console.log(++ans);
+console.log(ans++);
